5dc7ec199c2338c72679356009cb4e58617076dc
divrawat <divrawat2001@gmail>
2024-07-02 00:05:47 +0530

    s0fvsf

---
+++ config.js
@@ -0,0 +1,248 @@
+
+export const DOMAIN = 'http://localhost:3000';
+// export const DOMAIN = 'https://www.thislifestartsasachildactor.in.net';
+
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>This Life Starts as a Child Actor</u></a> is a compelling novel that delves into the tumultuous journey of a young actor navigating the complexities of fame and identity. Set against the backdrop of Hollywood, it follows the protagonist's rise from innocence to stardom, exploring the sacrifices and challenges along the way. `;
+
+export const MANGA_NAME = 'This Life Starts as a Child Actor';
+export const MANGA_AUTHOR = 'Kim Yoo';
+export const MANGA_RELEASE = '2018';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Kim Yoo';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Action, Adventure, Drama, Supernatural';
+
+export const CHAPTER_PREFIX = 'this-life-starts-as-a-child-actor-chapter';
+export const IMAGE_PREFIX = 'images/this-life-starts-as-a-child-actor';
+export const NEXT_PREVIOUS_PREFIX = 'manga/this-life-starts-as-a-child-actor-chapter';
+export const URL_PREFIX = 'manga/this-life-starts-as-a-child-actor-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/this-life-starts-as-a-child-actor-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://manhwatop.com/wp-content/uploads/2024/03/this-life-starts-as-a-child-actor-read-new-chapter-34-on-zinmanga.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[75px] rounded-full mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'This Life Starts as a Child Actor';
+export const NavbarName = "TLSAACA";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. Alex Summers, a young actor catapulted into the glittering world of Hollywood at an early age. As Alex navigates the highs and lows of fame, the novel explores their personal growth, relationships, and the challenges of maintaining authenticity amidst the pressures of stardom. Through Alex's experiences, the story delves into themes of ambition, identity, and the transformative power of success in the entertainment industry.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "This Life Starts as a Child Actor chronicles the captivating journey of a young protagonist thrust into the dazzling yet daunting world of Hollywood. From the innocence of childhood, the narrative unfolds as the character discovers a talent for acting that propels them into the spotlight at a tender age. As fame beckons, the novel delves deep into the complexities of navigating early success, revealing the protagonist's struggles with identity, relationships, and the relentless pressures of the entertainment industry."
+    },
+    {
+        id: 2,
+        content: "Set against the backdrop of glitzy Hollywood, the story paints a vivid picture of both the glamour and the darker realities behind the scenes. It explores the sacrifices made by the protagonist and their family, the highs of achievement juxtaposed with the lows of personal turmoil. Themes of ambition, integrity, and the quest for authenticity resonate throughout, as the protagonist grapples with fame's transformative effects on their sense of self."
+    },
+    {
+        id: 3,
+        content: "The novel intricately weaves together moments of triumph and adversity, portraying the protagonist's evolution from a wide-eyed child star to a mature artist navigating adulthood in the public eye. Relationships with family, mentors, and peers play pivotal roles, showcasing the impact of fame on personal connections and the delicate balance between professional success and personal fulfillment."
+    },
+    {
+        id: 4,
+        content: "Through its richly drawn characters and poignant storytelling, This Life Starts as a Child Actor offers a profound exploration of the human experience within the entertainment industry. It challenges perceptions of celebrity culture while capturing the universal themes of growth, resilience, and the quest for identity. Ultimately, the novel invites readers to ponder the price of fame and the profound journey of self-discovery that lies at the heart of the protagonist's narrative arc."
+    },
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 12
+    }
+]
---
+++ pages/robots.txt.js
@@ -0,0 +1,20 @@
+import { DOMAIN } from "../config";
+
+const generateRobotsTxt = () => {
+    let robotsTxtContent = `User-agent: *
+Disallow: /images/
+Sitemap: ${DOMAIN}/sitemap.xml
+`;
+    return robotsTxtContent;
+};
+
+export const getStaticProps = async ({ res }) => {
+    const robotsTxtContent = generateRobotsTxt();
+    res.setHeader("Content-Type", "text/plain");
+    res.write(robotsTxtContent);
+    res.end();
+    return { props: {} }
+}
+
+const RobotsTxt = () => { return null; };
+export default RobotsTxt;
